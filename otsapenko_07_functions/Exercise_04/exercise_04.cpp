07c76449428e7e53b1a58d2747a350fd485cb629
Alex Tsapenko <otsapenko@gmail.com>
2013-05-23 17:51:03 +0300

    Ex#4

---
+++ otsapenko_07_functions/Exercise_04/exercise_04.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+long double probability(unsigned numbers, unsigned picks); 
+
+int main() 
+{ 
+	using namespace std; 
+	double total, choices; 
+
+	cout << "Enter the total number of choices on the game card and\n" 
+	"the number of picks allowed:\n"; 
+	while ((cin >> total >> choices) && choices <= total) 
+	{ 
+		cout << "You have one chance in "; 
+		
+		double result = probability(total, choices);
+
+		cout << result;
+		cout << " of winning a prize. \n"; 
+		
+		cout << "Now enter the number of picks for meganumber:\n";
+		
+		while ((cin >> total) && 1 <= total)
+		{
+			cout << "You have one chance in "; 
+		
+			cout << probability(total, 1) * result;
+			cout << " of winning a main prize. \n"; 
+		}
+		
+		cout << "Next two numbers (q to quit) : "; 
+	} 
+
+	cout << "bye\n"; 
+	return 0; 
+
+} 
+
+long double probability(unsigned numbers, unsigned picks) 
+{ 
+	long double result = 1.0; 
+	long double n; 
+	unsigned p; 
+	
+	for (n = numbers, p = picks; p > 0; n--, p--) 
+		result = result * n / p ; 
+	
+	return result; 
+} 
